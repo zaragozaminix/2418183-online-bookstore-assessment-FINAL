0cfdccbd1424706965f2e4b50fde061cc0f346dc
zaragozaminix <zaragozaminix@gmail.com>
2025-10-16 23:31:01 +0100

    Fix(ci): Explicitly name test files to bypass discovery

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-PYTHONPATH=. python -m pytest tests/
+set -ex
+
+# Bypassing discovery by explicitly naming the test files
+PYTHONPATH=. python -m pytest tests/test_models.py tests/test_app.py
